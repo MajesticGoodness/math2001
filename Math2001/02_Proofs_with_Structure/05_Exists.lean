84a0977ba83867e42eba856510698c3b12cf3f8f
Kelvin R. <sariok@zoho.com>
2023-12-14 18:11:45 -0500

    Finished up Exercise 6! Jeez haha.

--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -192,8 +192,249 @@ example (x : ℚ) : ∃ y : ℚ, y ^ 2 > x := by
   --   _ = x / 2 - 3 / 2 + (x ^2 / 4 + 1 / 4) := by ring --idk where i was going with this lmao
   --   _ = ((x + 1) / 2) ^ 2 - 3 / 2 := by ring -- might be a clue, who knows.
 
+/- Cleaned up proof of Exercise 6 in section 2.5.9. -/
+
 example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
-  sorry
+
+  obtain ⟨x, h0⟩ := h
+  have H := le_or_gt x 1
+  obtain h1 | h1 := H
+
+  · have h2 :=
+      calc
+      0 < - x * t - 1 + x + t := by addarith [h0]
+      _ = (1 - x) * (t - 1) := by ring
+
+    have h3 :=
+      calc
+      1 - x ≥ 1 - 1 := by rel [h1]
+      _ = 0 := by ring
+
+    cancel 1 - x at h2
+    apply ne_of_gt
+    addarith [h2]
+
+  · have h2 :=
+      calc
+      0 < - x * t - 1 + x + t := by addarith [h0]
+      _ = (x - 1) * (1 - t) := by ring
+
+    have h3 :=
+      calc
+      x - 1 > 1 - 1 := by rel [h1]
+      _ = 0 := by ring
+
+    cancel x - 1 at h2
+    apply ne_of_lt
+    addarith [h2]
+
+/-
+This is my raw proof. It has a bunch of uneccessary filler, but I chose to
+leave it here for posterity.
+-/
+example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
+  obtain ⟨x, h0⟩ := h
+  have ayy := le_or_gt x 1
+
+  obtain sheesh | sheesh := ayy
+  · have h1 :=
+      calc
+      x + t = x + t := by ring
+      _ ≤ 1 + t := by rel [sheesh]
+      _ = 1 + t := by ring
+    have h11 : x * t + 1 - x - t < 0 := by addarith [h0]
+    have h111 :=
+      calc
+      0 > x * t + 1 - x - t := by rel [h11]
+      _ = (x - 1) * (t - 1) := by ring
+    have h1111 :=
+      calc
+       -0 < -((x - 1) * (t - 1)) := by rel [h111]
+       _ = (1 - x) * (t - 1) := by ring
+    have h1337 :=
+      calc
+      (1 - x) * (t - 1) > -0 := by rel [h1111]
+      _ = 0 := by ring
+    have h2 :=
+      calc
+      x * t + 1 < x + t := by rel[h0]
+      _ ≤ 1 + t := by rel [h1]
+    have h22 : x * t < t := by addarith [h2]
+    have h222 : x * t - t < 0 := by addarith [h22]
+    have h2222 :=
+      calc
+      t - x * t > 0 := by addarith [h222]
+      _ = 0 := by ring
+    have h3 :=
+      calc
+      1 - x < t - x * t := by addarith [h0]
+      _ = t * (1 - x) := by ring
+    have h4 :=
+      calc
+      1 - x = 1 - x := by ring
+      _ ≥ 1 - 1 := by rel [sheesh]
+      _ = 0 := by ring
+    --cancel 1 - x at h3
+    have h5 :=
+      calc
+      t * (1 - x) > 1 - x := by rel [h3]
+      _ ≥ 1 - 1 := by rel [sheesh]
+      _ = 0 := by ring
+
+    cancel 1 - x at h1337
+
+    have h6 : 1 < t := by addarith [h1337]
+
+    apply ne_of_gt
+
+    apply h6
+
+  · have h1 :=
+      calc
+      0 > x * t + 1 - x - t := by addarith [h0]
+      _ = (x - 1) * (t - 1) := by ring
+    have h2 :=
+      calc
+      x - 1 = x - 1 := by ring
+      _ > 1 - 1 := by rel [sheesh]
+      _ = 0 := by ring
+    have h1111 :=
+      calc
+       -0 < -((x - 1) * (t - 1)) := by rel [h1]
+       _ = (x - 1) * (1 - t) := by ring
+    have h1337 :=
+      calc
+      (x - 1) * (1 - t) > -0 := by rel [h1111]
+      _ = 0 := by ring
+    have h13 : 0 < (x - 1) * (1 - t) := by rel [h1337]
+
+    cancel x - 1 at h13
+    apply ne_of_lt
+
+    calc
+      t < 1 := by addarith [h13]
+
+/- Failed Attempts for Exercise 6 of section 2.5.9 -/
+
+/-
+My intuition told me that dividing the case around 1 was probably a good idea. I ran
+into trouble applying the 'cancel' tactic though, so I assumed I was wrong and abandoned the proof.
+Then, I tried breaking the case around '0' and I was able to prove the first case, when x ≤ 0.
+The second case, when x > 0, though proved impossible for me to prove. I had to spin in place for
+quite a while before I realized this. I was able to prove the first case, and now all that was left
+was to prove the second case, and I was determined to not give up, because after all, I was half of
+the way there. Ultimately, I had to swallow my disappointment and abandon the proof. Then, I tried
+to break the cases around 1 again, like I originally thought. This time I was successful in
+proving not just the first case, but the second case as well.
+
+It turns out that the cancel tactic is pretty finicky to use! It expects the expression to be
+in a very particular form before letting you apply it. Your thinking is probably correct, you just
+have to get in the right form.
+-/
+
+-- example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
+--   obtain ⟨x, h0⟩ := h
+--   have ayy := le_or_gt x 1
+
+--   obtain sheesh | sheesh := ayy
+--   · have h1 :=
+--       calc
+--       x + t = x + t := by ring
+--       _ ≤ 1 + t := by rel [sheesh]
+--       _ = 1 + t := by ring
+--     have h2 :=
+--       calc
+--       x * t + 1 < x + t := by rel[h0]
+--       _ ≤ 1 + t := by rel [h1]
+--     have h22 : x * t < t := by addarith [h2]
+--     have h222 : x * t - t < 0 := by addarith [h22]
+--     have h2222 :=
+--       calc
+--       t - x * t > 0 := by addarith [h222]
+--       _ = 0 := by ring
+--     have h3 :=
+--       calc
+--       1 - x < t - x * t := by addarith [h0]
+--       _ = t * (1 - x) := by ring
+--     have h4 :=
+--       calc
+--       1 - x = 1 - x := by ring
+--       _ ≥ 1 - 1 := by rel [sheesh]
+--       _ = 0 := by ring
+--     --cancel 1 - x at h3
+--     have h5 :=
+--       calc
+--       t * (1 - x) > 1 - x := by rel [h3]
+--       _ ≥ 1 - 1 := by rel [sheesh]
+--       _ = 0 := by ring
+--     cancel 1 - x at h5
+--     have h6 :=
+--       calc
+--       x * t + 1 ≤ (1) * t + 1 := by rel [sheesh]
+--       _ = t + 1 := by ring
+
+-- example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 := by
+--   obtain ⟨x, h0⟩ := h
+--   have ayy := le_or_gt x 0
+
+--   obtain sheesh | sheesh := ayy
+--   · have h1 :=
+--       calc
+--       x + t = x + t := by ring
+--       _ ≤ 0 + t := by rel [sheesh]
+--       _ = t := by ring
+--     have h11 : x * t + 1 - x - t < 0 := by addarith [h0]
+--     have h111 :=
+--       calc
+--       0 > x * t + 1 - x - t := by rel [h11]
+--       _ = (x - 1) * (t - 1) := by ring
+--     have h1111 :=
+--       calc
+--        -0 < -((x - 1) * (t - 1)) := by rel [h111]
+--        _ = (1 - x) * (t - 1) := by ring
+--     have h1337 :=
+--       calc
+--       (1 - x) * (t - 1) > -0 := by rel [h1111]
+--       _ = 0 := by ring
+--     have h2 :=
+--       calc
+--       x * t + 1 < x + t := by rel[h0]
+--       _ ≤ t := by rel [h1]
+--     have h22 : x * t < t := by addarith [h2]
+--     have h222 : x * t - t < 0 := by addarith [h22]
+--     have h2222 :=
+--       calc
+--       t - x * t > 0 := by addarith [h222]
+--       _ = 0 := by ring
+--     have h3 :=
+--       calc
+--       1 - x < t - x * t := by addarith [h0]
+--       _ = t * (1 - x) := by ring
+--     have h4 :=
+--       calc
+--       1 - x = 1 - x := by ring
+--       _ ≥ 1 - 0 := by rel [sheesh]
+--       _ = 1 := by ring
+--     --cancel 1 - x at h3
+--     have h5 :=
+--       calc
+--       t * (1 - x) > 1 - x := by rel [h3]
+--       _ ≥ 1 - 0 := by rel [sheesh]
+--       _ = 1 := by ring
+--     cancel 1 - x at h1337
+--     have h6 : 1 < t := by addarith [h1337]
+--     apply ne_of_gt
+--     apply h6
+
+--   · have h1 :=
+--       calc
+--       0 > x * t + 1 - x - t := by addarith [h0]
+--       _ = (x - 1) * (t - 1) := by ring
+--     have h2 :=
+--       calc
+--       x - 1 = x - 1 := by ring
+--       _ > 0 - 1 := by rel [sheesh]
+--       _ = - 1 := by ring
 
 example {m : ℤ} (h : ∃ a, 2 * a = m) : m ≠ 5 := by
   sorry
