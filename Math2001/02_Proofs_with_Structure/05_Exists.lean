bf8de5c94939d30f223480e76326fe4715f65993
Kelvin R. <sariok@zoho.com>
2023-12-25 16:46:39 -0500

    Finished exercise 9. Added exercise 8 from upstream.

--- Math2001/02_Proofs_with_Structure/05_Exists.lean
+++ Math2001/02_Proofs_with_Structure/05_Exists.lean
@@ -646,7 +646,204 @@ from the start.
 --   apply ne_of_lt
 --   apply h2
 
-example {a b c : ℝ} (ha : a ≤ b + c) (hb : b ≤ a + c) (hc : c ≤ a + b)
-    (ha' : 0 ≤ a) (hb' : 0 ≤ b) (hc' : 0 ≤ c) :
+
+/- Naive attempt at exercise 8, omegalul -/
+-- example {n : ℤ} : ∃ a, 2 * a ^ 3 ≥ n * a + 7 := by
+--   have h0 := le_or_succ_le n 0
+--   use 2
+--   have ay :=
+--     calc
+--     16 ≥ 7 := by numbers
+--     _ = 7 := by ring
+--   obtain h1 | h1 := h0
+--   · have h00 :=
+--       calc
+--       n * 2 + 7 ≤ (0) * 2 + 7 := by rel [h1]
+--       _ = 7 := by ring
+--     have h2 :=
+--       calc
+--       (2 * 2 ^ 3 : ℤ) = 16 := by ring
+--       _ ≥ 7 := by numbers
+--       _ ≥ n * 2 + 7 := by rel [h00]
+--     apply h2
+--   · have h00 :=
+--       calc
+--       1 * 2 + 7 ≤ n * 2 + 7 := by rel [h1]
+
+example {n : ℤ} : ∃ a, 2 * a ^ 3 ≥ n * a + 7 := by
+  use n
+  have h0 := le_or_succ_le n 0
+  obtain h1 | h1 := h0
+  have nauhgt :=
+    calc
+    n ^ 2 = n * n := by ring
+    _ ≥ 0 * 0 := by rel [h1]
+
+/- Exercise 9 of section 2.5.9 -/
+/-
+I knew something wonky would be happening here with the signs, I just didn't
+know how it would work out. I was totally stumped for a while, and nothing seemed to jump
+out at me as an obvious subsitution for x, y, and z.
+
+I wrote the following down on scratch paper:
+
+a = y + z
+b = x + z
+c = x + y
+
+I noticed how z appeared in the first two equations, but not the last. I started
+to think of z as an adjustor term. Something that could be added to an expression
+to get the result we're looking for. I know it sounds silly, but thinking in this way is
+what actually enabled me to press forward. I was completely paralyzed trying to find
+the right combination of terms that prove all three equations.
+
+I wrote down:
+
+a = a + 0 (let y be a, let z be 0)
+b = b + 0 (let x be b, let z be 0)
+c = a + b (because it's defined to be x + y)
+
+This was another kind of silly thing I did, which clearly wasn't a solution, but it clued me
+into the fact that the last equation was special somehow. I needed to make sure that, whichever
+a + b I chose, they had to somehow equal c when summed. The first two equations seemed 'easier'
+to work with, because I had an 'adjustor' term of z which I could use to get the result I needed
+from those two equations.
+
+Up until this point, my approach consisted of trying to find a combination that worked for the
+first equation, then finding another combination that worked for the second, then I would get
+hard-stuck on the third. The combinations I had previously chosen worked for the first two
+equations, but they trapped me into an impossible situation for the third. The scratch work
+I wrote up above encouraged me to flip my approach and start working with the last equation
+first, at least then the 'hard' part would be done, and I would be free to find a z term that would
+'adjust' the first two equations into something useful. Again, silly, because the x and y
+terms can also be thought of in this way, but this is the thinking that allowed the solution to
+'fall through' for me.
+
+I wrote down:
+
+x = (1/2) * a - (1/2) * b + (1/2) * c
+y = -(1/2) * a + (1/2) * b + (1/2) * c
+
+This works for the last equation! If we sum these two equations, we get c, like we want.
+
+Then I just started looking for a z that would give me what I wanted for the first two equations:
+
+z = (1/2) * a + (1/2) * b - (1/2) * c
+
+This was easy to find because I knew I wanted to use the z term to 'adjust' the first two equations
+in such a way that the c term was always eliminated, so I knew the sign on the c term would be
+negative for sure. The signs for a and b were easy to see that they would work out, because I
+could just compare them with what I wrote down for x and y.
+
+One last thing: the x and y I wrote down on in my scratch work are actually flipped to what
+I ended up using in the proof. In my proof, x is actually y, and y is x.
+
+-/
+
+/- Cleaned up proof for exercise 9, section 2.5.9 -/
+example {a b c : ℝ} (ha : a ≤ b + c) (hb : b ≤ a + c) (hc : c ≤ a + b) :
     ∃ x y z, x ≥ 0 ∧ y ≥ 0 ∧ z ≥ 0 ∧ a = y + z ∧ b = x + z ∧ c = x + y := by
-  sorry
+
+    use -(1/2) * a + (1/2) * b + (1/2) * c
+    use (1/2) * a - (1/2) * b + (1/2) * c
+    use (1/2) * a + (1/2) * b - (1/2) * c
+
+    constructor
+
+    · have x1 :=
+        calc
+        -(1 / 2) * a + 1 / 2 * b + 1 / 2 * c = (1/2) * (-a + b + c) := by ring
+        _ ≥ (1/2) * (-(b + c) + b + c) := by rel [ha]
+        _ = (1/2) * (0) := by ring
+        _ = 0 := by ring
+      apply x1
+
+    constructor
+
+    · have y1 :=
+        calc
+        1 / 2 * a - 1 / 2 * b + 1 / 2 * c = (1/2) * (a - b + c) := by ring
+        _ ≥ (1/2) * (a - (a + c) + c) := by rel [hb]
+        _ = 0 := by ring
+      apply y1
+
+    constructor
+
+    · have z1 :=
+        calc
+        1 / 2 * a + 1 / 2 * b - 1 / 2 * c = (1/2)  * (a + b - c) := by ring
+        _ ≥ (1/2) * (a + b - (a + b)) := by rel [hc]
+        _ = 0 := by ring
+      apply z1
+
+    constructor
+    ring
+    constructor
+    ring
+    ring
+
+
+
+/- Raw, uncleaned proof for exercise 9 of section 2.5.9 -/
+example {a b c : ℝ} (ha : a ≤ b + c) (hb : b ≤ a + c) (hc : c ≤ a + b) :
+    ∃ x y z, x ≥ 0 ∧ y ≥ 0 ∧ z ≥ 0 ∧ a = y + z ∧ b = x + z ∧ c = x + y := by
+
+    have huh :=
+      calc
+      a ≤ b + c := by rel [ha]
+      _ ≤ (a + c) + c := by rel [hb]
+      _ = a + 2 * c := by ring
+
+    have duh :=
+      calc
+      c ≤ a + b := by rel [hc]
+      _ ≤ (b + c) + b := by rel [ha]
+      _ = 2 * b + c := by ring
+
+    have bruh :=
+      calc
+      b ≤ a + c := by rel [hb]
+      _ ≤ a + (a + b) := by rel [hc]
+      _ = 2 * a + b := by ring
+
+    have huh1 : 0 ≤ c := by addarith [huh]
+    have duh1 : 0 ≤ b := by addarith [duh]
+    have bruh1 : 0 ≤ a := by addarith [bruh]
+
+    use -(1/2) * a + (1/2) * b + (1/2) * c
+    use (1/2) * a - (1/2) * b + (1/2) * c
+    use (1/2) * a + (1/2) * b - (1/2) * c
+
+    constructor
+
+    · have x1 :=
+        calc
+        -(1 / 2) * a + 1 / 2 * b + 1 / 2 * c = (1/2) * (-a + b + c) := by ring
+        _ ≥ (1/2) * (-(b + c) + b + c) := by rel [ha]
+        _ = (1/2) * (0) := by ring
+        _ = 0 := by ring
+      apply x1
+
+    constructor
+
+    · have y1 :=
+        calc
+        1 / 2 * a - 1 / 2 * b + 1 / 2 * c = (1/2) * (a - b + c) := by ring
+        _ ≥ (1/2) * (a - (a + c) + c) := by rel [hb]
+        _ = 0 := by ring
+      apply y1
+
+    constructor
+
+    · have z1 :=
+        calc
+        1 / 2 * a + 1 / 2 * b - 1 / 2 * c = (1/2)  * (a + b - c) := by ring
+        _ ≥ (1/2) * (a + b - (a + b)) := by rel [hc]
+        _ = 0 := by ring
+      apply z1
+
+    constructor
+    ring
+    constructor
+    ring
+    ring
